d12a434ffd9437064ef6c00daace16051a50207b
iojuedioe9rd <catsstudio81@gmail.com>
2023-10-14 16:59:23 +0100

    hi

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,7 @@ import './css/style.css'
 // Log message to console
 logMessage('Its finished!!')
 
+logMessage(process)
+
 if ((module as any).hot)       // eslint-disable-line no-undef
   (module as any).hot.accept() // eslint-disable-line no-undef
